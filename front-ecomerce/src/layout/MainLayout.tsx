9090af0b0b2fa2fa90799fb387b9b38e1ecd73e6
Juanma7882 <juanmaf236@gmail.com>
2026-01-27 13:50:21 -0300

    elimine la importacion de lmain de layout mainLayout

--- front-ecomerce/src/layout/MainLayout.tsx
+++ front-ecomerce/src/layout/MainLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import main from "../main"
 export default function MainLayout() {
     return (
         <>
